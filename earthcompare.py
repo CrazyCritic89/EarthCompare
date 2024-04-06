762585c6ed702254e980adc3a68ec82784c8fba2
CrazyCritic89 <cxrx5000@gmail.com>
2024-04-06 11:30:42 -0500

    Tweaked numbers to be more accurate

--- earthcompare.py
+++ earthcompare.py
@@ -53,7 +53,7 @@ for i in resj["bodies"][index]["atmosphereComposition"]: #This is awful
             atm3cp = 0.25
         case "Argon":
             atmos3 = resj["bodies"][index]["atmosphereComposition"]["Argon"]
-            atm3cp = 0.9
+            atm3cp = 0.93
 
 compo1 = resj["bodies"][index]["solidComposition"]["Rock"]
 compo2 = resj["bodies"][index]["solidComposition"]["Metal"]
@@ -61,15 +61,15 @@ compo2 = resj["bodies"][index]["solidComposition"]["Metal"]
 rotper = resj["bodies"][index]["rotationalPeriod"]
 
 massDif =   100 - (abs(1 - mass)        / ((1 + mass)        / 2)) * difRatio * 100
-radiusDif = 100 - (abs(6378 - radius)   / ((6378 + radius)   / 2)) * difRatio * 100
-gravityDif =100 - (abs(9.807 - gravity) / ((9.807 + gravity) / 2)) * difRatio * 100
-tempDif =   100 - (abs(288.15 - temp)   / ((288.15 + temp)   / 2)) * difRatio * 100
-presDif =   100 - (abs(1 - pres)        / ((1 + pres)        / 2)) * difRatio * 100
-atmos1Dif = 100 - (abs(77.9 - atmos1)   / ((77.9 + atmos1)   / 2)) * difRatio * 100
-atmos2Dif = 100 - (abs(20.9 - atmos2)   / ((20.9 + atmos2)   / 2)) * difRatio * 100
+radiusDif = 100 - (abs(6378.1 - radius)   / ((6378.1 + radius)   / 2)) * difRatio * 100
+gravityDif =100 - (abs(0.9999686429450564 - gravity) / ((0.9999686429450564 + gravity) / 2)) * difRatio * 100
+tempDif =   100 - (abs(288 - temp)   / ((288 + temp)   / 2)) * difRatio * 100
+presDif =   100 - (abs(0.9990787688132248 - pres)        / ((0.9990787688132248 + pres)        / 2)) * difRatio * 100
+atmos1Dif = 100 - (abs(77.89 - atmos1)   / ((77.89 + atmos1)   / 2)) * difRatio * 100
+atmos2Dif = 100 - (abs(20.89 - atmos2)   / ((20.89 + atmos2)   / 2)) * difRatio * 100
 atmos3Dif = 100 - (abs(atm3cp - atmos3) / ((atm3cp + atmos3) / 2)) * difRatio * 100
 compo1Dif = 100 - (abs(70.0 - compo1)   / ((70.0 + compo1)   / 2)) * difRatio * 100
 compo2Dif = 100 - (abs(30.0 - compo2)   / ((30.0 + compo2)   / 2)) * difRatio * 100
-rotperDif = 100 - (abs(1 - rotper)      / ((1 + rotper)      / 2)) * difRatio * 100
+rotperDif = 100 - (abs(0.997269752199074 - rotper)      / ((0.997269752199074 + rotper)      / 2)) * difRatio * 100
 
 print("It is " + str(round(((massDif+radiusDif+gravityDif+tempDif+presDif+atmos1Dif+atmos2Dif+atmos3Dif+compo1Dif+compo2Dif+rotperDif)/11), 2)) + "% like Earth")
