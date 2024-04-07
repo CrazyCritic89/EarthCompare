f446e24234b5d3ffea32dea0c41135417a7e6ddd
CrazyCritic89 <cxrx5000@gmail.com>
2024-04-07 12:17:33 -0500

    Should work with more composition types now

--- earthcompare.py
+++ earthcompare.py
@@ -44,17 +44,32 @@ gravity = resj["bodies"][index]["gravity"]
 temp = resj["bodies"][index]["surfaceTemperature"]
 pres = resj["bodies"][index]["surfacePressure"]
 
-atmos1 = resj["bodies"][index]["atmosphereComposition"]["Nitrogen"]
-atmos2 = resj["bodies"][index]["atmosphereComposition"]["Oxygen"]
+atmos = []
+atmcp = []
 
 for i in resj["bodies"][index]["atmosphereComposition"]: #This is awful
     match i:
+        case "Nitrogen":
+            atmos.append(resj["bodies"][index]["atmosphereComposition"][i])
+            atmcp.append(77.89)
+        case "Oxygen":
+            atmos.append(resj["bodies"][index]["atmosphereComposition"][i])
+            atmcp.append(20.89)
         case "Water":
-            atmos3 = resj["bodies"][index]["atmosphereComposition"]["Water"]
-            atm3cp = 0.25
+            atmos.append(resj["bodies"][index]["atmosphereComposition"][i])
+            atmcp.append(0.25)
         case "Argon":
-            atmos3 = resj["bodies"][index]["atmosphereComposition"]["Argon"]
-            atm3cp = 0.93
+            atmos.append(resj["bodies"][index]["atmosphereComposition"][i])
+            atmcp.append(0.93)
+        case _:
+            atmos.append(resj["bodies"][index]["atmosphereComposition"][i])
+            atmcp.append(1)
+
+print(len(atmos))
+
+if len(atmos) < 3:
+    atmos.append(3)
+    atmcp.append(1)
 
 compo1 = resj["bodies"][index]["solidComposition"]["Rock"]
 compo2 = resj["bodies"][index]["solidComposition"]["Metal"]
@@ -66,9 +81,9 @@ radiusDif = 100 - (abs(6378.1 - radius)   / ((6378.1 + radius)   / 2)) * difRati
 gravityDif =100 - (abs(0.9999686429450564 - gravity) / ((0.9999686429450564 + gravity) / 2)) * difRatio * 100
 tempDif =   100 - (abs(288 - temp)   / ((288 + temp)   / 2)) * difRatio * 100
 presDif =   100 - (abs(0.9990787688132248 - pres)        / ((0.9990787688132248 + pres)        / 2)) * difRatio * 100
-atmos1Dif = 100 - (abs(77.89 - atmos1)   / ((77.89 + atmos1)   / 2)) * difRatio * 100
-atmos2Dif = 100 - (abs(20.89 - atmos2)   / ((20.89 + atmos2)   / 2)) * difRatio * 100
-atmos3Dif = 100 - (abs(atm3cp - atmos3) / ((atm3cp + atmos3) / 2)) * difRatio * 100
+atmos1Dif = 100 - (abs(atmcp[0] - atmos[0])   / ((atmcp[0] + atmos[0])   / 2)) * difRatio * 100
+atmos2Dif = 100 - (abs(atmcp[1] - atmos[1])   / ((atmcp[1] + atmos[1])   / 2)) * difRatio * 100
+atmos3Dif = 100 - (abs(atmcp[2] - atmos[2])   / ((atmcp[2] + atmos[2]) / 2)) * difRatio * 100
 compo1Dif = 100 - (abs(70.0 - compo1)   / ((70.0 + compo1)   / 2)) * difRatio * 100
 compo2Dif = 100 - (abs(30.0 - compo2)   / ((30.0 + compo2)   / 2)) * difRatio * 100
 rotperDif = 100 - (abs(0.997269752199074 - rotper)      / ((0.997269752199074 + rotper)      / 2)) * difRatio * 100
